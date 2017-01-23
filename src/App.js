02317f076f7c6f63d26bf596477b37680044eeb3
Gabriel Diaz <gdiaz1410@gmail.com>
2017-01-22 20:50:00 -0600

    definitions rendering on page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-    constructor() {
-      super();
+  constructor() {
+    super();
 
-      this.state = {
-        word: '',
-        definitions: []
-      }
+    this.state = {
+      word: '',
+      definitions: []
     }
+  }
 
-    _getWordOfTheDay() {
-      axios.get('http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then((res) => {
-        let data = res.data;
-        console.log(data);
-        this.setState({ word: data.word, definitions: data.definitions });
-      });
-     }
+  _getWordOfTheDay() {
+    axios.get('http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then((res) => {
+      let data = res.data;
+      console.log(data);
+      this.setState({word: data.word, definitions: data.definitions});
+    });
+  }
 
-    componentWillMount() {
-      this._getWordOfTheDay();
+  _getDefinition() {
+    let defs = [];
+    let definitions = this.state.definitions;
+
+    for (var i = 0; i < definitions.length; i++) {
+      defs.push(
+        <p className="definition" key={i}>{definitions[i].text}</p>
+      );
     }
+    return <div>{defs}</div>;
+  }
 
-    render() {
+  componentWillMount() {
+    this._getWordOfTheDay();
+  }
+
+  render() {
+
+    let definitions = this._getDefinition();
 
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>{this.state.word}</h2>
+          <div>{definitions}</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
