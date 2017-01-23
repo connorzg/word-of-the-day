7a4936e2f1a4a74d9422b33c2be8b302d223b0d1
Connor Garber <czaneg@gmail.com>
2017-01-22 19:42:00 -0600

    GET and render word of the day

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
+    constructor() {
+      super();
+
+      this.state = {
+        word: '',
+        definitions: []
+      }
+    }
+
+    _getWordOfTheDay() {
+      axios.get('http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then((res) => {
+        let data = res.data;
+        console.log(data);
+        this.setState({ word: data.word, definitions: data.definitions });
+      });
+     }
+
+    componentWillMount() {
+      this._getWordOfTheDay();
+    }
+
+    render() {
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{this.state.word}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
